f95fde08112c4c24e7a61e793d22214c0bdd0535
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-02-20 21:01:07 -0600

    load local storage works

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ $(document).ready(function () {
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
-
   $(function () {
+    // TODO: Add code to display the current date in the header of the page.
+    $("#currentDay").text(dayjs().format('dddd, MMMM D'))
+    // TODO: Add code to apply the past, present, or future class to each time
+    //  block by comparing the id to the current hour. HINTS: How can the id
+    //  attribute of each time-block be used to conditionally add or remove the
+    //  past, present, and future classes? How can Day.js be used to get the
+    //  current hour in 24-hour time?
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -16,18 +23,15 @@ $(document).ready(function () {
       localStorage.setItem(key, text);
     })
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    //  block by comparing the id to the current hour. HINTS: How can the id
-    //  attribute of each time-block be used to conditionally add or remove the
-    //  past, present, and future classes? How can Day.js be used to get the
-    //  current hour in 24-hour time?
-
     // TODO: Add code to get any user input that was saved in localStorage and set
     //  the values of the corresponding textarea elements.
     // HINT: How can the id attribute of each time-block be used to do this?
-    $("#hour-9").localStorage.getItem('hour-9')
-
-    // TODO: Add code to display the current date in the header of the page.
-    $("#currentDay").text(dayjs().format('dddd, MMMM D'))
+    $(".saveBtn").each(function () {
+      var storedKey = $(this).parent().attr('id')
+      var storedText = localStorage.getItem(storedKey);
+      if (storedText) {
+        $(this).siblings(".description").text(storedText);
+      }
+    })
   });
 });
